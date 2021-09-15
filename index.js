21fa7e8742303737063015ca8112c440318b9ff4
pye <pyethradelcolribeiro@gmail.com>
2021-09-15 01:49:04 -0300

    atualizacao

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 
-var palestrantes = (`Prof Rodrigo`, `Prof Marcelo`)
-var idade =0
-var quantidadeParticipantes =0
+var palestrantes=(`Prof Rodrigo`, `Prof Marcelo`)
+var alunos=(`Regina`, `Teresa`, `Paola`)
+var idade=0
+var quantidadeParticipantes=0
 
 while(quantidadeParticipantes<100, quantidadeParticipantes+1){
-    console.log(`cadastro Ok`)
+    console.log(palestrantes)
+    console.log(alunos)
+}
+
+if(quantidadeParticipantes<100){
+
 }
