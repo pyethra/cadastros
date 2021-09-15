4bea3df84d2e3ad8a65e626dd636b4131b25cbde
pye <pyethradelcolribeiro@gmail.com>
2021-09-15 01:43:07 -0300

    Tentativa sozinha

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var palestrantes = (`Prof Rodrigo`, `Prof Marcelo`)
+var idade =0
+var quantidadeParticipantes =0
+
+while(quantidadeParticipantes<100, quantidadeParticipantes+1){
+    console.log(`cadastro Ok`)
+}
