0ce051324dd3f5e59a306ad88f488597a8276773
pye <pyethradelcolribeiro@gmail.com>
2021-09-26 16:42:22 -0300

    final

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 let palestrantes = (`prof Richard e prof Marcelo`)
+let participantes = []
 let dataHoje = `2021-09-21`
 let dataEvento = `2021-10-05`
 let idadeAluno = 20
 let nomeAluno = `Henrique`
 let cadastroCompleto = true
-let totalVagas = '100'
+let totalVagas = 100
 
 if (idadeAluno < 18) {
     console.log(`Desculpa, evento exclusivo para maiores de 18 anos`)
@@ -22,14 +23,17 @@ if (nomeAluno.length < 4) {
     cadastroCompleto = false
 }
 
-if (cadastroCompleto == false) {
+if (!cadastroCompleto) {
     console.log(`Oh oh, aconteceu um erro, verifique as informações acima`)
-}
-
-else {
+} else {
+    participantes.push(nomeAluno)
     console.log(`Cadastro completo`)
-    console.log(`Bem vindo a imersao Front End`)
+    console.log(`Bem vindo a imersao Front End,`, nomeAluno)
     console.log(`Apresentando nossos palestrantes,`, palestrantes)
 }
-
-for (let i = 0; i < totalVagas.length; i++) {;}
+    for (let i = 0; i < participantes.length; i++) {
+        console.log('Apresentando os participantes')
+}  
+    for (let i = 0; i < participantes.length; i++) {
+        console.log(participantes[i])
+}
